e576ae569a56156db9dd7aa9ff34c89e21492519
Coding Forest <64945652+Coding-Forest@users.noreply.github.com>
2021-11-17 17:32:05 +0900

    Add files via upload

---
+++ 1 Advanced/React FCC 12 Multiple Inputs.js
@@ -0,0 +1,102 @@
+import React, { useState, Fragment} from 'react';
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+// dynamic object keys
+
+const ControlledInputs = () => {
+/* Individual multiple inputs
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email, age};
+      console.log(person);
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty values');
+    }
+  };
+  */
+
+  // Multiple inputs in an object
+
+  const [person, setPerson] = useState({firstName:'', email:'', age:''});
+  const [people, setPeople] = useState([]); 
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setPerson({...person, [name]:value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(person.firstName && person.email && person.age){
+      const newPerson = {...person, id:new Date().getTime().toString()}
+      setPeople([...people, newPerson])
+      setPerson({firstName:'', email:'', age:''})
+    }
+  };
+
+  return (
+    <Fragment>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name : </label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              value={person.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email : </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              value={person.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+          <label htmlFor='age'>Age : </label>
+          <input
+            type='age'
+            id='age'
+            name='age'
+            value={person.age}
+            onChange={handleChange}
+          />
+        </div>
+          <button type='submit' onClick={handleSubmit}>add person</button>
+        </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className='item' key={id}>
+              <h4>{person.firstName}</h4>
+              <p>{person.age}</p>
+              <p>{person.email}</p>
+            </div>
+          );
+        })}
+      </article>
+    </Fragment>
+  );
+};
+
+export default ControlledInputs;
